b927331abc119255965023ce0f0d5ed3a68c8052
ucler <ucler@DESKTOP-25JG5UL>
2021-01-20 23:50:49 +0300

    20 Ocak Ders Sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin temel Kurs";
+            string kurs3 = "Java Başlangıç temel Kurs";
+            string kurs4 = "Python Kursu";
+
+            //array - dizi
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı" , "Programlamaya Başlangıç İçin temel Kurs", "Java Başlangıç temel Kurs", "Python Kursu" };
+
+
+            for (int i = 0; i <kurslar.Length; i++) 
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            foreach (string kurs in kurslar)  //Dizi yapılı temaları tek tek dönmeye yarar
+            {
+                Console.WriteLine(kurs);
+            }
+
+
+
+
+
+            Console.WriteLine("Sayfa Sonu - footer");
+        }
+    }
+}
---
+++ Kampİntro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini Tekrarlama
+            string kategoriEtiketi = "Kategori"; //Değişken atamak için kullnılır
+            int ogrenciSayisi = 32000;  //Tam Sayılar
+            double faizOrani = 1.45;   //Ondalıklı Sayılar
+            bool SistemeGirisYapmıisMi = true;  //Kontrol Kodu
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu Göster");
+            }
+
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu Göster");
+            }
+
+            if (SistemeGirisYapmıisMi == true)  //Şart Bloğu
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
